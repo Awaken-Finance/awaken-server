10ea250dbf072b4ed4bbc30c167b3cfd38d0e7ed
zoe121212 <jklmnopqr6@awaken.finance>
2024-05-20 20:08:23 +0800

    feat: Integrate Aetherlink token price service

--- src/AwakenServer.Grains/AwakenServerGrainsModule.cs
+++ src/AwakenServer.Grains/AwakenServerGrainsModule.cs
@@ -1,14 +1,23 @@
-﻿using Volo.Abp.AutoMapper;
+﻿using Aetherlink.PriceServer;
+using Aetherlink.PriceServer.Common;
+using Aetherlink.PriceServer.Options;
+using AwakenServer.Grains.Grain.Tokens.TokenPrice;
+using Microsoft.Extensions.DependencyInjection;
+using Volo.Abp.AutoMapper;
 using Volo.Abp.Modularity;
 
 namespace AwakenServer.Grains;
 
-[DependsOn(typeof(AwakenServerDomainModule), typeof(AwakenServerApplicationContractsModule))]
+[DependsOn(typeof(AwakenServerDomainModule), 
+    typeof(AwakenServerApplicationContractsModule),
+    typeof(AetherlinkPriceServerModule))]
 public class AwakenServerGrainsModule : AbpModule
 {
     public override void ConfigureServices(ServiceConfigurationContext context)
     {
         base.ConfigureServices(context);
         Configure<AbpAutoMapperOptions>(options => options.AddMaps<AwakenServerGrainsModule>());
+        
+        context.Services.AddSingleton<ITokenPriceProvider, TokenPriceAetherlinkProvider>();
     }
 }
