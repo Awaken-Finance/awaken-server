d367c6cab7e2b59ffecd893b0d15929031604fdf
zoe121212 <jklmnopqr6@awaken.finance>
2024-05-23 10:01:00 +0800

    cleanup: remove deprecated code

--- src/AwakenServer.Application/Trade/TradePairAppService.cs
+++ src/AwakenServer.Application/Trade/TradePairAppService.cs
@@ -464,43 +464,7 @@ namespace AwakenServer.Trade
                 return null;
             }
         }
-
-        public async Task UpdateTotalSupplyAsync(Guid id, string chainId)
-        {
-            var grain = _clusterClient.GetGrain<ITradePairGrain>(GrainIdHelper.GenerateGrainId(id));
-            var token = await GetTokenInfoAsync(id, chainId);
-            var supply = token != null ? token.Supply.ToDecimalsString(token.Decimals) : "0";
-            if (supply == "0")
-            {
-                _logger.LogError($"update total supply, trade pair: {id}, get token info failed");
-                return;
-            }
-            
-            var result = await grain.UpdateTotalSupplyAsync(supply);
-            
-            if (!result.Success)
-            {
-                _logger.LogError($"update total supply, updage grain {id} failed");
-                return;
-            }
-            
-            _logger.LogInformation($"update total supply, publishAsync TradePairEto: {JsonConvert.SerializeObject(result.Data.TradePairDto)}");
-
-            await _distributedEventBus.PublishAsync(new EntityCreatedEto<TradePairEto>(
-                _objectMapper.Map<TradePairGrainDto, TradePairEto>(
-                    result.Data.TradePairDto)
-            ));
-
-            if (result.Data.SnapshotDto != null)
-            {
-                _logger.LogInformation($"update total supply, publishAsync TradePairMarketDataSnapshotEto: {JsonConvert.SerializeObject(result.Data.SnapshotDto)}");
-            
-                await _distributedEventBus.PublishAsync(new EntityCreatedEto<TradePairMarketDataSnapshotEto>(
-                    _objectMapper.Map<TradePairMarketDataSnapshotGrainDto, TradePairMarketDataSnapshotEto>(
-                        result.Data.SnapshotDto)
-                ));
-            }
-        }
+        
         
         public async Task UpdateTradePairAsync(Guid id)
         {
