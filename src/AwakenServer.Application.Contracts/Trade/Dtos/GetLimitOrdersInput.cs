adb45f7fdaf2674c3a3a3b2ec5048a90512c5e64
zoe121212 <jklmnopqr6@awaken.finance>
2024-08-14 14:20:43 +0800

    feat: fix limit order status

--- src/AwakenServer.Application.Contracts/Trade/Dtos/GetLimitOrdersInput.cs
+++ src/AwakenServer.Application.Contracts/Trade/Dtos/GetLimitOrdersInput.cs
@@ -1,4 +1,6 @@
 using System.ComponentModel.DataAnnotations;
+using System.Runtime.Serialization;
+using System.Text.Json.Serialization;
 using Volo.Abp.Application.Dtos;
 
 namespace AwakenServer.Trade.Dtos;
@@ -13,10 +15,21 @@ public class GetLimitOrdersInput : PagedAndSortedResultRequestDto
 
 public enum LimitOrderStatus
 {
+    [EnumMember(Value = "COMMITTED")]
     Committed = 1,
+    
+    [EnumMember(Value = "PARTIALLY_FILLING")]
     PartiallyFilling = 2,
+    
+    [EnumMember(Value = "FULL_FILLED")]
     FullFilled = 3,
+    
+    [EnumMember(Value = "CANCELLED")]
     Cancelled = 4,
+    
+    [EnumMember(Value = "EXPIRED")]
     Expired = 5,
+    
+    [EnumMember(Value = "REVOKED")]
     Revoked = 6
 }
