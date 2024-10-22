b0447307b63b22bed3a5a446c2d15c2c71d74470
zoe121212 <jklmnopqr6@awaken.finance>
2024-10-22 17:42:06 +0800

    feat: clean code

--- src/AwakenServer.Domain/Entities/IMultiChain.cs
+++ src/AwakenServer.Domain/Entities/IMultiChain.cs
@@ -1,5 +1,4 @@
 using System;
-using Orleans;
 
 namespace AwakenServer.Entities
 {
--- src/AwakenServer.Domain/Entities/MultiChainEntity.cs
+++ src/AwakenServer.Domain/Entities/MultiChainEntity.cs
@@ -1,14 +1,12 @@
 using System;
 using Nest;
-using Orleans;
 
 namespace AwakenServer.Entities
 {
-    [GenerateSerializer]
     public class MultiChainEntity<TKey> : AwakenEntity<TKey>, IMultiChain
     {
         [Keyword]
-        [Id(0)] public virtual string ChainId { get; set; }
+        public virtual string ChainId { get; set; }
 
 
         protected MultiChainEntity()
